3931b906d5bf856949685460f0b64c245785310c
Lucas de Arimateia Coutinho da Silva <93049848+LuscasArimate@users.noreply.github.com>
2023-05-29 16:12:44 -0300

    Add files via upload

---
+++ AboutUS/script.js
@@ -0,0 +1,25 @@
+$(".testmonial_slider_area").owlCarousel({
+    autoplay: true,
+    slideSpeed:1000,
+    items : 3,
+    loop: true,
+    nav:true,
+    navText:['<i class="fa fa-arrow-left"></i>','<i class="fa fa-arrow-right"></i>'],
+    margin: 30,
+    dots: true,
+    responsive:{
+        320:{
+            items:1
+        },
+        767:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+    
+});
